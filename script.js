5c75170f19bc2d185cbdf03011234f1016802009
GerardLeal <92707646+GerardLeal@users.noreply.github.com>
2021-12-15 20:31:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed('.typing',{
+    strings:["Adamsonian Student","Gamer", "Basketball Player", "Guitarist"],
+    backSpeed: 60,
+    typeSpeed: 100,
+    loop: true,
+  });
+
+  var typed = new Typed('.typing2',{
+    strings:["Adamsonian Student","Gamer", "Basketball Player", "Guitarist"],
+    backSpeed: 60,
+    typeSpeed: 100,
+    loop: true,
+  });
+  
+
+
